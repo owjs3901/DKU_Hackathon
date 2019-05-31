133dfa4b5430b2993443af619173a38aa911d378
ojm <owjs3901@naver.com>
2019-05-31 23:02:19 +0900

    add koreaMap layout

--- app/src/main/java/com/spdd/MainActivity.java
+++ app/src/main/java/com/spdd/MainActivity.java
@@ -17,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         m=this;
+        findViewById(R.id.search).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(m,MenuActivity.class));
+            }
+        });
         findViewById(R.id.qr).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
