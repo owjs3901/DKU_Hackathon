4cc540d4f57aaab6aa1b1b57f041e32b0c53fe4d
ojm <owjs3901@naver.com>
2019-06-01 02:33:20 +0900

    update food activity

--- app/src/main/java/com/spdd/MainActivity.java
+++ app/src/main/java/com/spdd/MainActivity.java
@@ -1,5 +1,6 @@
 package com.spdd;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -28,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.food).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(m,FoodActivity.class));
+                startActivity(new Intent(m,FoodActivity.class), ActivityOptions.makeSceneTransitionAnimation(m).toBundle());
             }
         });
 
